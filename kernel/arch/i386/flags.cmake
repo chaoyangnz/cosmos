3ebb9b41294c030bba6663668fe85331cf0517ee
Chao Yang <chaoyang.nz@gmail.com>
2019-04-18 21:17:59 +1200

    refine CMake

--- kernel/arch/i386/flags.cmake
+++ kernel/arch/i386/flags.cmake
@@ -1,3 +1,3 @@
-set(ISA_ASM_FLAGS "-g -F dwarf -f elf")
+set(ISA_ASM_FLAGS "-f elf")
 set(ISA_C_FLAGS "-m32 -std=gnu99 -ffreestanding -fno-exceptions -nostdlib -fno-stack-protector -Og -Wall -Wextra")
 set(ISA_LINKER_FLAGS "")
